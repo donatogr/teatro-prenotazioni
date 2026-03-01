ecb1d5c86c0156f19d00f4c909fc2a993ccb5d14
donato <donato.gr@gmail.com>
2026-03-01 20:51:51 +0100

    Railway: nginx in ascolto su PORT, .gitattributes LF per .sh

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -e
 export BACKEND_URL="${BACKEND_URL:-http://backend:5000}"
-envsubst '${BACKEND_URL}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+export PORT="${PORT:-80}"
+envsubst '${BACKEND_URL} ${PORT}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 exec nginx -g "daemon off;"
